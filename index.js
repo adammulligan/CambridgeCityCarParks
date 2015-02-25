b87395db371b1b9faec6e232d772bb29c29684cd
Adam Mulligan <adammulligan@fastmail.fm>
2015-02-25 22:01:44 +0000

    Run every 5 mins

--- index.js
+++ index.js
@@ -12,24 +12,37 @@ var bunyan = require('bunyan'),
 var request = require('request-promise'),
     cheerio = require('cheerio');
 
+var Agenda = require('agenda'),
+    agenda = new Agenda({db: { address: 'localhost:27017/car-park-agenda'}});
+
 var API_URL = "https://www.cambridge.gov.uk/sites/all/modules/jdi/modules/jdi_parking_ajax/car_park_status.php?p=complete";
 var COUNT_REGEX = /(\d+) spaces \((.*)\)/;
 
-request(API_URL).then( function(body) {
-  var $ = cheerio.load(body);
+var retrieveData = function(job, done) {
+  request(API_URL).then( function(body) {
+    var $ = cheerio.load(body);
 
-  $('h2').each( function() {
-    var lot_name = $(this).text();
-    var lot_info = $(this).next('p').text()
+    $('h2').each( function() {
+      var lot_name = $(this).text();
+      var lot_info = $(this).next('p').text()
 
-    var matches = COUNT_REGEX.exec(lot_info);
-    var spaces_available = matches[1];
-    var extra_info = matches[2];
+      var matches = COUNT_REGEX.exec(lot_info);
+      var spaces_available = matches[1];
+      var extra_info = matches[2];
 
-    log.info({
-      "name": lot_name,
-      "spaces_available": spaces_available,
-      "extra_info": extra_info
+      log.info({
+        "name": lot_name,
+        "spaces_available": spaces_available,
+        "extra_info": extra_info
+      });
+
+      done();
     });
   });
-});
+};
+
+log.info('Starting up');
+
+agenda.define('retrieve parking space data', retrieveData);
+agenda.every('5 minutes', 'retrieve parking space data');
+agenda.start();
