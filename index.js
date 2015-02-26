3f73b2065b795031666b7232da38cab48cb2d48d
Adam Mulligan <adammulligan@fastmail.fm>
2015-02-26 14:50:02 +0000

    Store everything in mongo
    Bunyan doesn't seem to append to logs...

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ var bunyan = require('bunyan'),
     log = bunyan.createLogger({
       name: 'cambridge_car_parks',
       streams: [{
-        type: 'file',
-        path: './car_parks.log'
-      }, {
         stream: process.stdout
       }]
     });
@@ -13,12 +10,15 @@ var request = require('request-promise'),
     cheerio = require('cheerio');
 
 var Agenda = require('agenda'),
-    agenda = new Agenda({db: { address: 'localhost:27017/car-park-agenda'}});
+    agenda = new Agenda({db: { address: 'localhost:27017/cambridge_car_parks-agenda'}});
+
+var MongoClient = require('mongodb').MongoClient;
 
 var API_URL = "https://www.cambridge.gov.uk/sites/all/modules/jdi/modules/jdi_parking_ajax/car_park_status.php?p=complete";
 var COUNT_REGEX = /(\d+) spaces \((.*)\)/;
+var MONGO_URL = 'mongodb://localhost:27017/cambridge_car_parks';
 
-var retrieveData = function(job, done) {
+var retrieveData = function(db, callback) {
   request(API_URL).then( function(body) {
     var $ = cheerio.load(body);
 
@@ -26,23 +26,32 @@ var retrieveData = function(job, done) {
       var lot_name = $(this).text();
       var lot_info = $(this).next('p').text()
 
+      if (lot_info === undefined) { return; }
+
       var matches = COUNT_REGEX.exec(lot_info);
       var spaces_available = matches[1];
       var extra_info = matches[2];
 
-      log.info({
+      var car_park_status = {
         "name": lot_name,
         "spaces_available": spaces_available,
         "extra_info": extra_info
-      });
+      };
 
-      done();
+      log.info(car_park_status);
+
+      var collection = db.collection(lot_name.toLowerCase().replace(/\s/g, "_"));
+      collection.insert(car_park_status, callback);
     });
   });
-};
+}
 
-log.info('Starting up');
+log.info('Connecting to Mongo');
 
-agenda.define('retrieve parking space data', retrieveData);
-agenda.every('5 minutes', 'retrieve parking space data');
-agenda.start();
+MongoClient.connect(MONGO_URL, function(err, db) {
+  log.info('Connected to mongo, starting up...');
+
+  agenda.define('retrieve parking space data', function(job, callback) { retrieveData(db, callback); });
+  agenda.every('10 seconds', 'retrieve parking space data');
+  agenda.start();
+});
